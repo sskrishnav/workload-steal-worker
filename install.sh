80c2eccaa06f1eab74e02f6ef025919a9ec8f33d
S S Krishna Vutukuri <sskrishnavutukuri@Krishnas-MacBook-Pro-M2.local>
2025-01-30 07:01:29 +0530

    Work load steal worker

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Build Docker image"
+docker build -t workloadstealworker:alpha1 .
+
+echo "Load Image to Kind cluster"
+kind load docker-image workloadstealworker:alpha1
 
 echo "Deploying Worker Server"
 kubectl apply -f deployment.yaml
