d49eb7d77176874f7c61fe6306ded39b0ab61da2
S S Krishna Vutukuri <sskrishnavutukuri@Krishnas-MacBook-Pro-M2.local>
2025-01-31 00:34:57 +0530

    Work load steal worker

--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log/slog"
 	"os"
+	"time"
 
 	nats "github.com/nats-io/nats.go"
 	corev1 "k8s.io/api/core/v1"
@@ -73,14 +74,45 @@ func (n *consume) Start(stopChan chan<- bool) error {
 			slog.Error("Failed to decode Pod from message", "error", err)
 			return
 		}
+		// Create the Pod in Kubernetes
+		createdPod, err := n.cli.CoreV1().Pods(pod.Namespace).Create(context.TODO(), &pod, metav1.CreateOptions{})
+		if err != nil {
+			slog.Error("Failed to create Pod", "error", err)
+			return
+		}
+		if !isPodSuccesfullyRunning(n.cli, pod.Namespace, pod.Name) {
+			slog.Info("Failed to stole the wrokload", "Pod", createdPod)
+		}
+		slog.Info("Succefully stole the wrokload", "Pod", createdPod)
 	})
+	select {}
+}
 
-	// Create the Pod in Kubernetes
-	createdPod, err := n.cli.CoreV1().Pods(pod.Namespace).Create(context.TODO(), &pod, metav1.CreateOptions{})
-	if err != nil {
-		slog.Error("Failed to create Pod", "error", err)
-		return err
+// pollPodStatus polls the status of a Pod until it is Running or a timeout occurs
+func isPodSuccesfullyRunning(clientset *kubernetes.Clientset, namespace, name string) bool {
+	timeout := time.After(5 * time.Minute)    // Timeout after 5 minutes
+	ticker := time.NewTicker(5 * time.Second) // Poll every 5 seconds
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-timeout:
+			slog.Error("Timeout waiting for Pod to reach Running state", "namespace", namespace, "name", name)
+			return false
+		case <-ticker.C:
+			pod, err := clientset.CoreV1().Pods(namespace).Get(context.TODO(), name, metav1.GetOptions{})
+			if err != nil {
+				slog.Error("Failed to get Pod status", "namespace", namespace, "name", name, "error", err)
+				return false
+			}
+
+			slog.Info("Pod status", "namespace", namespace, "name", name, "phase", pod.Status.Phase)
+
+			// Check if the Pod is Running
+			if pod.Status.Phase == corev1.PodRunning {
+				slog.Info("Pod is now Running", "namespace", namespace, "name", name)
+				return true
+			}
+		}
 	}
-	slog.Info("Succefully stole the wrokload", "Pod", createdPod)
-	return nil
 }
